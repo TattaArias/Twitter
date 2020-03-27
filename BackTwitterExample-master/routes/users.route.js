39ac52e3fadf40a1e2400196f5e59500e7f838ed
javargasgue <55053065+javargasgue@users.noreply.github.com>
2020-03-27 17:23:48 -0500

    Se agregan rutas para los usuarios

--- BackTwitterExample-master/routes/users.route.js
+++ BackTwitterExample-master/routes/users.route.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 const userController = require ('../controllers/user.controller'); 
 /**
+ * POST Route to create user
+ */
+router.post ('/',userController.createUser);
+/**
  * GET Route to list all users
  */
 router.get('/', userController.findAllUsers);
@@ -9,10 +13,6 @@ router.get('/', userController.findAllUsers);
  * GET Route to find user by id
  */
 router.get('/:idUser', userController.findOneUser);
-/**
- * POST Route to create user
- */
-router.post ('/',userController.createUser);
 /**
  * PUT Route to update an user by id
  */
@@ -25,13 +25,11 @@ router.delete ('/:username',userController.deleteUserByUsername);
  * DELETE Route to delete all users
  */
 router.delete ('/',userController.deleteAllUsers);
-
-
 /**
- * TASK:
- * ADD THE MISSING ROUTES ______________________________________________________ 
+ * find All Users By CreatedDate
  */
+router.get('/date/:create_date', userController.findAllUsersByCreatedDate);
 
-// Export router
+ // Export router
 module.exports = router;
 

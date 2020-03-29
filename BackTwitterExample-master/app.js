b6f1d079c8074246d8620873812901a3bc1009f0
javargasgue <55053065+javargasgue@users.noreply.github.com>
2020-03-29 16:20:02 -0500

    Se modifica
    Se agrega información de de tweets y rutas

--- BackTwitterExample-master/app.js
+++ BackTwitterExample-master/app.js
@@ -8,6 +8,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users.route');
 var postsRouter = require('./routes/posts.route');
+var tweetsRouter = require('./routes/tweets.route');
+var followersRouter = require('./routes/followers.route');
 
 
 // IMPORT DB CONNECTION MANAGER
@@ -25,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
+app.use('/tweets', tweetsRouter);
+app.use('/followers', followersRouter);
 
 
 /**

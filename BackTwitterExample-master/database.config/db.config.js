dc9b0c296007f4416d2050ed2fd20a15ff4153ce
javargasgue <55053065+javargasgue@users.noreply.github.com>
2020-03-29 16:04:21 -0500

    Se modifica
    Se deja sin contraseña ya que, se evidencia que el error presentado se debía a la no inicialización del servidor. Se inicializa por medio de XAMPP

--- BackTwitterExample-master/database.config/db.config.js
+++ BackTwitterExample-master/database.config/db.config.js
@@ -4,7 +4,7 @@
 const dbconfig = {
         HOST: "localhost",
         USER: "root", 
-        PASSWORD: "Julian2019*", 
+        PASSWORD: "", //Julian2019*
         DB: "faketwitter",
         dialect: "mysql",
         pool: {
